e573f36c4b61757e2cab7c594a11b19be84a4db8
umair <umair.mahmood22@gmail.com>
2014-12-16 23:38:05 +0500

    add jquery file in assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,12 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap.min
 //= require bootstrapnew.min
 //= require highcharts.js
 //= require exporting.js
-//= require jquery
 //= require tinymce
 //= require tinymce-jquery
 //= require_tree .
