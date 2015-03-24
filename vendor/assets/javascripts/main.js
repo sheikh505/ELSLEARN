aebb20cb30cbd543682c9a09f1c4f3be223f77ca
umair <umair.mahmood22@gmail.com>
2015-03-24 23:11:09 +0500

    els front page changes
    Signed-off-by: umair <umair.mahmood22@gmail.com>

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -1,8 +1,1 @@
-	$(document).ready(function() {
-    $(".tabs").hide();
-    $(".fa-caret-down").hide();
-    $(".header_col").click(function(){
-            $(this).next().children(".tabs").slideToggle(500);
-            $(this).find(".fa-caret-right, .fa-caret-down").toggle();
-    });
-});
+
