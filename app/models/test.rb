63c2762e2d5f29edb8aead912a800dd9ba05f039
fahad.k.bhatti@gmail.com <fahad.k.bhatti@gmail.com>
2015-03-30 16:25:44 +0500

    test removed from the app

--- app/models/test.rb
+++
@@ -1,8 +0,0 @@
-class Test < ActiveRecord::Base
-  belongs_to :course
-  belongs_to :user
-  has_many :questions
-
-  # attr_accessible :title, :body
-  attr_accessible :degree_course_id, :test_code, :name, :user_id, :question_ids
-end
