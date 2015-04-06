94f230fd995f38b766f40a6fbf18cf2700473959
Farhat Baig <farhat.baig@46labs.com>
2015-04-06 18:01:14 +0500

    test

--- db/migrate/20150303122346_remove_degree_course_assignment_id_from_books.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDegreeCourseAssignmentIdFromBooks < ActiveRecord::Migration
-  def up
-    remove_column :books, :degree_course_assignment_id
-  end
-
-  def down
-    add_column :books, :degree_course_assignment_id, :int
-  end
-end
