fb1f7d0dd012de3107267af7f5db417bd9a97b9c
fahad.k.bhatti@gmail.com <fahad.k.bhatti@gmail.com>
2015-04-06 23:18:20 +0500

    updated by me...

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,19 +3,9 @@ class UserController < ApplicationController
   end
 
   def save_result
-    @board_id = params[:b_id]
-    @degree_id = params[:degree_id]
-    @course_id = params[:course_id]
-    @mcq = params[:mcq]
-    @fill = params[:fill]
-    @true_false = params[:true_false]
-    @descriptive = params[:descriptive]
-    @past_paper_flag = params[:pre_Past]
-    @year = params[:year]
-    @session = params[:session]
-    UserTestHistory.new(params[:user_test_history])
-    UserTestHistory.save
-    redirect_to root_path
+    user_history = UserTestHistory.new(params[:user_test_history])
+    user_history.save
+    redirect_to user_my_profile_path
   end
 
   def my_profile
