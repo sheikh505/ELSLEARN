9681238efe564d6e63be2b6d58ef5879786736a0
Farhat Baig <farhat.baig@46labs.com>
2015-06-10 21:55:49 +0500

    new layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 //= require masonry
 //= require jquery.flexslider-min
 //= require dataTables/jquery.dataTables
+//= require jquery.inview.min
 
 //= require main
 //= require_tree .
@@ -34,12 +35,9 @@ $(document).ready(function() {
         $(this).next().children(".tabs").slideToggle(500);
         $(this).find(".fa-caret-right, .fa-caret-down").toggle();
     });
-    var $container = $('#container');
-// initialize
-    $container.masonry({
-        columnWidth: 200,
-        itemSelector: '.box'
-    });
+    $('.mobil_btn').click( function(){
+        $('.nav').slideToggle('fast')
+    })
     $(function(){
         //SyntaxHighlighter.all();
     });
@@ -51,18 +49,59 @@ $(document).ready(function() {
             }
         });
     });
-    $('.flexslider .slides li').hover(function(){
-        $(".slider_content").animate({
-            opacity: "1"
-        }, {
-            queue: false
-        });
-    }, function () {
-        $(".slider_content").animate({
-            opacity: "0"
-        }, {
-            queue: false
-        });
+    //$('.flexslider .slides li').hover(function(){
+      //  $(".slider_content").animate({
+        //    opacity: "1"
+        //}, {
+          //  queue: false
+        //});
+    //}, function () {
+      //  $(".slider_content").animate({
+        //    opacity: "0"
+        //}, {
+          //  queue: false
+        //});
+    //});
+
+});
+
+$(document).ready(function() {
+    //Animated Progress
+    $('.progress-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).css('width', $(this).data('width') + '%');
+            $(this).unbind('inview');
+        }
     });
 
+    //Animated Number
+    $.fn.animateNumbers = function(stop, commas, duration, ease) {
+        return this.each(function() {
+            var $this = $(this);
+            var start = parseInt($this.text().replace(/,/g, ""));
+            commas = (commas === undefined) ? true : commas;
+            $({value: start}).animate({value: stop}, {
+                duration: duration == undefined ? 1000 : duration,
+                easing: ease == undefined ? "swing" : ease,
+                step: function() {
+                    $this.text(Math.floor(this.value));
+                    if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                },
+                complete: function() {
+                    if (parseInt($this.text()) !== stop) {
+                        $this.text(stop);
+                        if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                    }
+                }
+            });
+        });
+    };
+
+    $('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        var $this = $(this);
+        if (visible) {
+            $this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+            $this.unbind('inview');
+        }
+    });
 });
