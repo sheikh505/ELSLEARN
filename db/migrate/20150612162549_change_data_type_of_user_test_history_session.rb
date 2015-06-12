8a6ce75d0ffc1c6a18734877eae9dd150faa60d1
Farhat Baig <farhat.baig@46labs.com>
2015-06-12 21:36:32 +0500

    migration updated

--- db/migrate/20150612162549_change_data_type_of_user_test_history_session.rb
+++ db/migrate/20150612162549_change_data_type_of_user_test_history_session.rb
@@ -1,12 +1,12 @@
 class ChangeDataTypeOfUserTestHistorySession < ActiveRecord::Migration
   def up
-    change_column :user_test_histories do |t|
+    change_table :user_test_histories do |t|
       t.change :session, :string
     end
   end
 
   def down
-    change_column :user_test_histories do |t|
+    change_table :user_test_histories do |t|
       t.change :session, :integer
     end
   end
