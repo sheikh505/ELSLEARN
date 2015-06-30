73d21203fdfa73f92769c653d34a942eff9728f7
fahad.k.bhatti@gmail.com <fahad.k.bhatti@gmail.com>
2015-07-01 01:42:12 +0500

    model updated

--- app/models/question_history.rb
+++ app/models/question_history.rb
@@ -1,4 +1,8 @@
 class QuestionHistory < ActiveRecord::Base
-  belongs_to :question, :board, :degree, :topic, :user
+  belongs_to :question
+  belongs_to :board
+  belongs_to :degree
+  belongs_to :topic
+  belongs_to :user
   attr_accessible :board_id, :degree_id, :difficulty, :is_approved, :question_id, :topic_id, :user_id
 end
