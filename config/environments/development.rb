f775ab3a84a8a50e98b83ab2e0fc46591e65ba82
[Touseef <touseefmurtaza1993@gmail.com>
2016-02-19 17:07:57 +0500

    imagemagick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,4 @@ ExamsSystem::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
