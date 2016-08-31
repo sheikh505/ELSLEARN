70bd3413ef9c9fdd146b80b6aa8ff779027d2cd6
fahad.k.bhatti@gmail.com <fahad.k.bhatti@gmail.com>
2016-08-31 21:54:34 +0500

    Option fixed for empty

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -177,7 +177,7 @@ class ServicesController < ApplicationController
       @questionlist = questions.map do |u|
         { :id=> u.id, :statement => u.statement, :type => u.question_type,
           :options =>  u.options.map do |o|
-            {:id => o.id, :question_id => o.question_id, :flag => o.flag, :image_url => o.avatar.url(:medium), :statement => o.statement, :is_answer => o.is_answer}
+            {:id => o.id, :question_id => o.question_id, :flag => o.flag, :image_url => o.avatar.url(:medium), :statement => o.statement.present? ? o.statement : "", :is_answer => o.is_answer}
           end
         }
       end
