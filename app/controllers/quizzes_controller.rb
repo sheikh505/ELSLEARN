1e1be31eb0bfbc974577642986b32b8010905c49
cell zone <cellzone@cells-MacBook-Pro.local>
2016-09-29 20:11:01 +0500

    Fix in get_questions and get_els_questions in Quiz controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -52,33 +52,25 @@ class QuizzesController < ApplicationController
 
   def get_questions
     @course_id = params[:course_id]
-
-    @questions = Question.select("questions.*,topics.name as topic_name,courses.name as course_name").
-        joins(:topic => :course).
-        where("course_id = ? and author != ? and workflow_state = 'accepted'", @course_id,current_user.email)
+    @questions = Question.select("questions.*").
+        joins(:course_linking).
+        where("(course_1  IN (?) OR course_2  IN (?) OR course_3  IN (?) OR course_4  IN (?)) and author != ? and workflow_state = 'accepted'", @course_id, @course_id, @course_id, @course_id,current_user.email)
     render :partial => 'quizzes/questions_list'
   end
 
   def get_els_questions
     @course_id = params[:course_id]
-    if params[:topic_id] == "" || params[:topic_id] == 'ALL'
-      @questions = Question.select("questions.*,topics.name as topic_name,courses.name as course_name").
-          joins(:topic => :course).
-          where("course_id = ? and workflow_state = 'accepted' and question_type in (?)", @course_id,params[:ques_types].split(","))
-    else
-      @questions = Question.select("questions.*,topics.name as topic_name,courses.name as course_name").
-          joins(:topic => :course).
-          where("course_id = ? and topic_id = ? and workflow_state = 'accepted' and question_type in (?)", @course_id,params[:topic_id],params[:ques_types].split(","))
-    end
+    @questions = Question.select("questions.*").
+        joins(:course_linking).
+        where("(course_1  IN (?) OR course_2  IN (?) OR course_3  IN (?) OR course_4  IN (?)) and workflow_state = 'accepted' and question_type in (?)", @course_id, @course_id, @course_id, @course_id,params[:ques_types].split(","))
     @questions.shuffle!
     if @questions.size == 0
-
+      render :json => {:success => false}.to_json
     else
       @question = @questions.pop
       ids = @questions.pluck(:id).shuffle.join(",")
       render :json => {:success => true,html: render_to_string(partial: 'quizzes/pop_up'),:question_ids => ids}.to_json
     end
-    render :json => {:success => false}.to_json
   end
 
   def test_exists
