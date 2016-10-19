c8227a3403575ac00a4269a80a185cbff0e574a7
cell zone <cellzone@cells-MacBook-Pro.local>
2016-10-19 20:31:15 +0500

    sign_in service fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     topic_id.nil? ? "":Topic.find(topic_id).name
 
   end
-  protect_from_forgery
+  protect_from_forgery with: :null_session
 
   def after_sign_in_path_for(resource)
     if resource.is_student?
